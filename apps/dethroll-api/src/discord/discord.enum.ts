5b5d6c1bf3bb64d1196b486f652bec0384b53935
goran9999 <gutovicgoran@gmail.com>
2023-11-18 12:37:32 +0300

    wip:registering bot commands

---
+++ apps/dethroll-api/src/discord/discord.enum.ts
@@ -0,0 +1,5 @@
+export enum DETHRollCommands {
+  InitDethroll = 'init_dethroll',
+  JoinGame = 'join_game',
+  Roll = 'roll',
+}
--- apps/dethroll-api/src/discord/discord.service.ts
+++ apps/dethroll-api/src/discord/discord.service.ts
@@ -4,12 +4,21 @@ import {
   Logger,
   OnModuleInit,
 } from '@nestjs/common';
-import { Client, Events, Partials, REST } from 'discord.js';
+import {
+  Client,
+  Events,
+  Interaction,
+  Partials,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+} from 'discord.js';
 import axios from 'axios';
 import { CreateUserDto } from '../user/dto/user.dto';
 import { generateDiscordPfPUrl } from '../utils/helpers';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from '../user/user.service';
+import { DETHRollCommands } from './discord.enum';
 @Injectable()
 export class DiscordService implements OnModuleInit {
   client: Client;
@@ -32,10 +41,10 @@ export class DiscordService implements OnModuleInit {
       this.client.once(Events.ClientReady, () => {
         this.logger.log(`Logged in as ${this.client.user!.username}!`);
       });
-
       this.client.on(Events.ThreadCreate, () => {});
-
       this.client.login(this.configService.get('DISCORD_TOKEN'));
+      this.registerSlashCommand();
+      this.interactionReact();
     } catch (error: any) {
       this.logger.error(error.message);
     }
@@ -45,6 +54,89 @@ export class DiscordService implements OnModuleInit {
     return {
       uri: this.configService.get('DISCORD_REDIRECT_URI') + wallet,
     };
+  }
+
+  registerSlashCommand() {
+    try {
+      (async () => {
+        try {
+          await this.rest.put(
+            Routes.applicationGuildCommands(
+              process.env.APPLICATION_ID!,
+              process.env.GUILD_ID!
+            ),
+            {
+              body: [
+                new SlashCommandBuilder()
+                  .setName('init_dethroll')
+                  .setDescription('Initialize new dETHroll game!')
+                  .addStringOption((option) =>
+                    option
+                      .setName('dETH bet')
+                      .setDescription('Amount of dETH currency to bet')
+                      .setRequired(true)
+                  ),
+
+                new SlashCommandBuilder()
+                  .setName('join_dethroll')
+                  .setDescription('Join dETHroll game!')
+                  .addStringOption((option) =>
+                    option
+                      .setName('Oponent')
+                      .setDescription('Amount of dETH currency to bet')
+                      .setRequired(true)
+                  ),
+
+                new SlashCommandBuilder()
+                  .setName('roll')
+                  .setDescription('Roll dETHroll number!')
+                  .addStringOption((option) =>
+                    option
+                      .setName('Oponent')
+                      .setDescription('Amount of dETH currency to bet')
+                      .setRequired(true)
+                  ),
+              ],
+            }
+          );
+          this.logger.verbose('Registered command');
+        } catch (error) {
+          this.logger.error('Failed to register command!' + error.message);
+        }
+      })();
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  interactionReact() {
+    try {
+      this.client.on('interactionCreate', (interaction) => {
+        if (!interaction.isCommand()) {
+          throw new Error('Inetaction is not command!');
+        }
+
+        const command = interaction.command;
+
+        switch (command.name) {
+          case DETHRollCommands.InitDethroll: {
+            this.initGameHandler(interaction);
+            break;
+          }
+          case DETHRollCommands.JoinGame: {
+            this.joinGameHandler(interaction);
+            break;
+          }
+          case DETHRollCommands.Roll: {
+            this.rollHandler(interaction);
+            break;
+          }
+          default: {
+            throw new BadRequestException('Command not supported');
+          }
+        }
+      });
+    } catch (error) {}
   }
 
   async getUserData(code: string, wallet: string) {
@@ -89,4 +181,10 @@ export class DiscordService implements OnModuleInit {
       throw new BadRequestException(error);
     }
   }
+
+  async initGameHandler(interaction: Interaction) {}
+
+  async joinGameHandler(interaction: Interaction) {}
+
+  async rollHandler(interaction: Interaction) {}
 }
